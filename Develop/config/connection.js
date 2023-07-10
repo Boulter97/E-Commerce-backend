6509f0006bd395d3fb0222b39661b70530f65206
Boulter97 <118101244+Boulter97@users.noreply.github.com>
2023-07-10 15:19:38 +1000

    mysql problems

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,10 +2,22 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+const config = {
+  database: {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER || 'boulter97',
+    password: process.env.DB_PASSWORD || '123',
+    database: process.env.DB_NAME || 'ecommerce_db'
+  },
+  
+};
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+  : new Sequelize(config.database.database, config.database.username, config.database.password, {
+      host: config.database.host,
+      port: config.database.port,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
